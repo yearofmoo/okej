92e4eafd21a7eaffe82d4b1b063a39760c0c9491
Matias Niemelä <matias@yearofmoo.com>
2023-05-31 12:14:09 +0200

    Make sure that err() calls allow for exception/Err instances to be passed in as a first param

--- src/err.ts
+++ src/err.ts
@@ -1,42 +1,48 @@
 import type { Err } from "./api";
 import { isJsError } from "./shared";
 
+// this is used to make sure that at least one of the
+// properties of an object is defined (becausePartial
+// makes all properties optional))
+type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> &
+  U[keyof U];
+
 /**
  * Creates an Err result.
  */
 export function err(): Err;
 export function err(err: null | undefined | boolean): Err;
 export function err<T extends Err>(err: T): T;
-export function err<N extends number>(errCode: N): Err<N>;
+export function err<C extends number>(errCode: C): Err<C>;
 export function err(errMessage: string): Err;
-export function err<C extends number | string>(
+export function err<C extends unknown = number | string>(
   errMessage: string,
   errCode: C,
 ): Err<C>;
 export function err<
-  X extends { [key: string]: unknown },
-  N extends unknown = string | number,
->(errMessage: string, errCode: N, errContext: X): Err<number, Error, X>;
+  C extends unknown = string | number,
+  X extends { [key: string]: unknown } = { [key: string]: unknown },
+>(errMessage: string, errCode: C, errContext: X): Err<number, Error, X>;
 export function err<
   E extends Err,
-  N extends unknown = string | number,
+  C extends unknown = string | number,
   X extends { [key: string]: unknown } = { [key: string]: unknown },
 >(
-  e: Partial<E> | unknown,
+  e: AtLeastOne<Partial<E>>,
   errMessage?: string,
-  errCode?: N,
+  errCode?: C,
   errContext?: X,
-): Err<N, NonNullable<E["errException"]>, X>;
+): Err<C, NonNullable<E["errException"]>, X>;
 export function err<
   E extends Error,
-  N extends number,
-  X extends { [key: string]: unknown },
+  C extends unknown = string | number,
+  X extends { [key: string]: unknown } = { [key: string]: unknown },
 >(
-  e: E | Partial<Err> | unknown,
+  e: E | { stack: string; message: string } | unknown,
   errMessage?: string,
-  errCode?: N,
+  errCode?: C,
   errContext?: X,
-): Err<N, E, X>;
+): Err<C, E, X>;
 export function err(a?: unknown, b?: unknown, c?: unknown, d?: unknown): Err {
   let code: number | string = 0;
   let message: string = "";
